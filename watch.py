ad0a780a33c9541e7743d4a24433ad359eb9b71b
mholgatem <holgate.mark1@gmail.com>
2015-03-18 09:11:03 -0700

    Update watch.py

--- watch.py
+++ watch.py
@@ -20,8 +20,8 @@ class EventHandler(FileSystemEventHandler):
             if event.event_type == 'created': 
                 self.info.append(self.c.execute('SELECT id FROM menu_items WHERE rom_path LIKE "{0}%"'.format(os.path.dirname(event.src_path))).fetchone()[0])
                 print 'new file found:', event.src_path
-                if event.event_type == 'deleted':
-                    self.info.append(self.c.execute('SELECT id FROM menu_items WHERE rom_path LIKE "{0}%"'.format(os.path.dirname(event.src_path))).fetchone()[0])
+            if event.event_type == 'deleted':
+                self.info.append(self.c.execute('SELECT id FROM menu_items WHERE rom_path LIKE "{0}%"'.format(os.path.dirname(event.src_path))).fetchone()[0])
                 print 'file deleted:', event.src_path
 	else: print
 
